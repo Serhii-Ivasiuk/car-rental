a437ffce6ddfa129ae86fb21f5bd84fc65e3fa62
Serhii-Ivasiuk <ivasiuk.sa@gmail.com>
2023-09-14 19:42:40 +0300

    fix: add abortController to data fetching

--- src/api/advertsApi.js
+++ src/api/advertsApi.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://64849eb7ee799e321626def8.mockapi.io';
 
-export const limit = 10;
+export const getAdverts = async (page = 1, limit = 10, signal = null) => {
+    const options = {
+        params: { limit, page },
+    };
 
-export const getAdverts = async (page = 1) => {
-    const { data } = await axios.get(`/adverts?limit=${limit}&page=${page}`);
+    if (signal) options.signal = signal;
+
+    const { data } = await axios.get('/adverts', options);
+
     return data;
 };
